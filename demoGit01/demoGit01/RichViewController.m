a4837c9495a41ed027019cc004bb5a913e05c4e6
iOS18 <ios18@mac10-2.local>
2013-09-30 19:51:08 +0700

    hehe comit lan dau tien vao repo

---
+++ demoGit01/demoGit01/RichViewController.m
@@ -0,0 +1,29 @@
+//
+//  RichViewController.m
+//  demoGit01
+//
+//  Created by iOS18 on 9/30/13.
+//  Copyright (c) 2013 iOS18. All rights reserved.
+//
+
+#import "RichViewController.h"
+
+@interface RichViewController ()
+
+@end
+
+@implementation RichViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ demoGit01/demoGit01/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  demoGit01
+//
+//  Created by iOS18 on 9/30/13.
+//  Copyright (c) 2013 iOS18. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RichAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RichAppDelegate class]));
+    }
+}
